d8442bc8f50bf0d0557d00ede0ee4ecea2604e32
piojeet <manjhipiojeet@gmail.com>
2024-07-16 00:57:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1400px"
+    },
+
+    extend: {
+      colors: {
+        bodyColor: "#f3f4f6",
+        lightGray: "#e5e7eb"
+      }
+    },
+
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "10px",
+        sm: "30px",
+      }
+    },
+
+    fontFamily: {
+      Montserat: ['Montserrat", sans-serif']
+    }
+  },
+  plugins: [],
+}
